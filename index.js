bcfc47c6add76ca856e0eafabdf70ff67ad4dd20
Mina <wilhelmina.jane@gmail.com>
2015-05-26 14:53:06 +0100

    implements searching for a word in an array

--- index.js
+++ index.js
@@ -2,16 +2,30 @@ var fs = require('fs');
 var ac = {};
 
 ac.import = function(callback){ // saving import fct as a property of ac{}
+  if(!callback || typeof callback !== 'function'){
+    //console.log('----FAIL!----');
+    return new Error('callback argument MUST be a function');
+  }
   var filename = __dirname + '/words.txt';
-  // fs.readFile(myFile, ['utf8'], callback)
-  fs.readFile(filename, 'utf8', function(err, data){
+  fs.readFile(filename, 'utf8', function(err, data){ // fs.readFile(myFile, ['utf8'], callback)
     if (err){
       console.log(err);
       console.log(data);
     }
     ac.words = data.split('\n'); // stores array of words in ac{}
-    callback(ac.words); // call callback function on ac.words
+    return callback(ac.words); // call callback function on ac.words
   });
+}
+
+ac.findWord = function (word, callback){
+  //var found = ac.words[ac.words.indexOf(word)];
+  var found = [];
+  for (var i = 0; i < ac.words.length; i++){
+    if (ac.words[i].search(word) === 0){
+      found.push(ac.words[i]);
+    }
+  }
+  return callback(null,found);
 }
 
 module.exports = ac;// exporting ac object to use elsewhere
--- test/test.js
+++ test/test.js
@@ -4,8 +4,37 @@ var ac = require('../'); //('../index[.js]')
 assert.equal(typeof ac, 'object');
 assert.equal(typeof ac.import, 'function');
 
-console.log("# ac.import imports a list of words into memory");
+console.log("#test: ac.import imports a list of words into memory");
 ac.import(function (words) {
   console.log("words.txt had " + words.length + " words in it");
   assert.equal(words.length, 235887);
 });
+
+console.log("#test: attempt to invoke ac.import without a valid callback");
+var error = ac.import('string');
+assert.equal(error.message, 'callback argument MUST be a function');
+
+
+
+ac.import(function(){
+  console.log('#test: ac.findWord finds a string in words array');
+  ac.findWord('awes', function (err, found){
+    assert.equal(err, null);
+    assert.equal(found.length, 4);
+  });
+})
+
+//index.js
+// ac.import is a function
+// this function has a parameter
+// this parameter is our callback function
+// ac.import does some stuff
+// at the end of ac.import, the callback is called
+// the callback is called with the stuff ac.import did
+
+//test.js
+// when we call ac.import, it takes a function
+// (this function plugs into callback)
+// this function will automatically have:
+// the stuff that was given to the callback (when we defined ac.import)
+// here, parameter passed to the callback function here is a placeholder/reference to the actual stuff we were given by ac.import
